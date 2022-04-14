dd831566c4ef78790a8641d8c61738f2e82e8d32
marcelompimenta <marcelopimenta.work@gmail.com>
2022-04-14 00:19:52 -0400

    created a module, logger.js that generates a .txt file with the memory stats logs.'

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const os = require('os')
+const log = require('./logger')
 
 const { freemem, totalmem } = os
 
@@ -17,6 +18,8 @@ setInterval(() => {
     }
     console.log("======= PC STATS ======")
     console.table(stats)
+
+    log(JSON.stringify(stats))
 
 }, 1000)
 
